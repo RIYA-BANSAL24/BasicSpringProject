54778431d5db47f499f07af192f26665cb06785f
RIYA_BANSAL24 <riyabansal3365@gmail.com>
2025-03-01 22:42:08 +0530

    Dependency Injection

--- src/main/java/com/SpringConcepts/SpringConceptsApplication.java
+++ src/main/java/com/SpringConcepts/SpringConceptsApplication.java
@@ -1,19 +1,28 @@
 package com.SpringConcepts;
 
 import com.SpringConcepts.component.DemoBean;
+import com.SpringConcepts.component.EmployeeBean;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com/SpringConcepts/component")
 @SpringBootApplication
 public class SpringConceptsApplication {
 	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept Demo");
-		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
-		System.out.println("Checking Context: " + context.getBean(DemoBean.class));
+		logger.debug("Welcome to My First Spring Project");
+		ApplicationContext context =SpringApplication.run (SpringConceptsApplication.class, args);
+		logger.debug("Checking Context: {}",context.getBean (DemoBean.class));
+		logger.debug("\n*** adding EmployeeBean and DepartmentBean ***");
+		EmployeeBean employeeBean =context.getBean(EmployeeBean.class);
+		employeeBean.setEid(100);
+		employeeBean.setEname("Spring Framework");
+		employeeBean.showEmployeeDetails();
 	}
 }
