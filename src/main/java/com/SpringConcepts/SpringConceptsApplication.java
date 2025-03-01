95006a09aff243559063b6729599ae0766977bf8
RIYA_BANSAL24 <riyabansal3365@gmail.com>
2025-03-01 22:31:57 +0530

    Log Messages

--- src/main/java/com/SpringConcepts/SpringConceptsApplication.java
+++ src/main/java/com/SpringConcepts/SpringConceptsApplication.java
@@ -1,12 +1,16 @@
 package com.SpringConcepts;
 
 import com.SpringConcepts.component.DemoBean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 	public static void main(String[] args) {
 		System.out.println("Welcome to Spring Concept Demo");
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
